20351845f12850caca3de3b413f489cfd57bdbc8
sandeep0009 <500sandeepks@gmail.com>
2025-02-07 23:32:39 +0530

    Feat:completed frontend

---
+++ apps/web/app/signin/page.tsx
@@ -0,0 +1,15 @@
+
+export const Page=()=>{
+    return(
+        <div className="min-h-screen flex items-center justify-center shadow-md border rounded-lg ">
+            <div className="w-full max-w-md">
+                <div>
+                    <h2>Chat Mono App</h2>
+                    <p className="text-slate-600 text-xm">Welcome to chatapp build on monorepo.Signup for more</p>
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
